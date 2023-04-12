076714fc476fe7c680775b192cd0bf41d3734ccc
Kilohoflipped <a1357425655@163.com>
2023-04-12 23:03:21 +0800

    重写

--- app/libs/laz_process/dsm_generator.py
+++ app/libs/laz_process/dsm_generator.py
@@ -1,101 +1,96 @@
 import numpy as np
 import laspy
-import os
 from PIL import Image
 from scipy import ndimage
 from scipy.interpolate import griddata
 
+
 class DSM:
-    def __init__(self, laz_files_dir, step_size):
+    def __init__(self, laz_files_path, step_size):
         self.laz_file = None
-        self.laz_files_dir = laz_files_dir
+        self.laz_files_path = laz_files_path
         self.step_size = step_size
-        self.points_all = self._read_laz_files()
-        self.bounds = ((np.min(self.points_all[:, 0]), np.max(self.points_all[:, 0])),
-                       (np.min(self.points_all[:, 1]), np.max(self.points_all[:, 1])))
+        self.points_all = None
+        self._read_laz_files()
 
-        self.zBounds = (np.min(self.points_all[:, 2]), np.max(self.points_all[:, 2]))
+        self.bounds = ((self.laz_file.header.x_min, self.laz_file.header.x_max),
+                       (self.laz_file.header.y_min, self.laz_file.header.y_max))
 
-        self.dsm = self._interpolate()
-        self.normalized_dsm = (self.dsm - np.min(self.dsm)) / (np.max(self.dsm) - np.min(self.dsm))
+        self.zBounds = (self.laz_file.header.z_min, self.laz_file.header.z_max)
+        self.heightRaster = None
+        self.heightRasterFilled = None
+        self._rasterize()
+
+        self.normalizedHeightRaster = np.zeros_like(self.heightRaster, dtype=float) + 255
+        self.normalizedHeightRaster[self.heightRaster > 0] = (self.heightRaster[self.heightRaster > 0] -
+                                                              np.min(self.heightRaster[self.heightRaster > 0])) / \
+                                                             (np.max(self.heightRaster[self.heightRaster > 0]) -
+                                                              np.min(self.heightRaster[self.heightRaster > 0]))
 
     def _read_laz_files(self):
         """
         从Laz文件中读取点云数据，并将其存储为Numpy数组
         """
-        points_all = np.zeros((0, 3))
-        for file in os.listdir(self.laz_files_dir):
-            if file.endswith('.laz'):
-                file_path = os.path.join(self.laz_files_dir, file)
-                self.laz_file = laspy.read(file_path)
-                points = self.laz_file.xyz
-                points_all = np.append(points_all, points, axis=0)
-        return points_all
+        self.points_all = np.zeros((0, 3))
+        self.laz_file = laspy.read(self.laz_files_path)
+        self.points_all = np.append(self.points_all, self.laz_file.xyz, axis=0)
 
-    def _interpolate(self):
+    def _rasterize(self):
         """
-        对点云进行插值处理，得到高程的栅格图像
+        对点云进行栅格化处理，得到高程的栅格图像
         """
         # 定义栅格的xy坐标
         (x_min, x_max), (y_min, y_max) = self.bounds
-        xi = np.arange(x_min, x_max + self.step_size, self.step_size)
-        yi = np.arange(y_min, y_max + self.step_size, self.step_size)
+        xi = np.arange(x_min, x_max + self.step_size, self.step_size)  # 创建栅格，并且保证做有点都能囊括进栅格中
+        yi = np.arange(y_min, y_max + self.step_size, self.step_size)  # 创建栅格，并且保证做有点都能囊括进栅格中
         XI, YI = np.meshgrid(xi, yi)
 
         # 计算每个点在栅格中的索引
-        x_indices = ((self.points_all[:, 0] - x_min) / self.step_size).astype(int)
+        x_indices = ((self.points_all[:, 0] - x_min) / self.step_size).astype(int)  # 求出点云的栅格坐标
         y_indices = ((self.points_all[:, 1] - y_min) / self.step_size).astype(int)
 
         # 在栅格中聚合和插值
-        raster = np.zeros_like(XI, dtype=float)
+        self.heightRaster = np.zeros_like(XI, dtype=float)
         count = np.zeros_like(XI, dtype=int)
-        np.add.at(raster, (y_indices, x_indices), self.points_all[:, 2])
-        np.add.at(count, (y_indices, x_indices), 1)
+        np.add.at(self.heightRaster, (y_indices, x_indices), self.points_all[:, 2])  # 对应值叠加，将所有高程值叠加到栅格中
+        np.add.at(count, (y_indices, x_indices), 1)  # 记录每个栅格内被叠加了多少个高程数据，用来计算平均值
         # 避免分母为0的情况
-        raster[count > 0] /= count[count > 0]
-
-        return raster
+        self.heightRaster[count > 0] /= count[count > 0]  # 计算栅格高程平均值
 
     def fill_no_data(self):
         """
         对于在原始点云数据中没有赋予值的栅格，通过插值方法进行赋值
         """
-        # 找到没有值的栅格
-        no_data_indices = np.isclose(self.dsm, 0, atol=0, rtol=1e-8)
+        # 获取已知值和位置的坐标
+        heightRasterSize = (self.heightRaster.shape[0], self.heightRaster.shape[1])
+        known_coords = np.argwhere(self.heightRaster != 0)
+        known_values = self.heightRaster[self.heightRaster != 0]
 
-        # 获取已知值的栅格和对应的坐标
-        known_values = self.dsm[~no_data_indices]
-        (x_min, x_max), (y_min, y_max) = self.bounds
-        x_coords, y_coords = np.meshgrid(np.arange(x_min, x_max + self.step_size, self.step_size),
-                                         np.arange(y_min, y_max + self.step_size, self.step_size))
-        known_coords = np.column_stack((x_coords[~no_data_indices].flatten(),
-                                        y_coords[~no_data_indices].flatten()))
+        # 生成整个矩阵的坐标网格
+        yi, xi = np.mgrid[0:heightRasterSize[0], 0:heightRasterSize[1]]
+        coords = np.vstack((yi.ravel(), xi.ravel())).T
 
-        # 获取未知值的栅格坐标
-        unknown_coords = np.column_stack((x_coords[no_data_indices].flatten(),
-                                          y_coords[no_data_indices].flatten()))
+        # 使用线性插值填充缺失值
+        self.heightRasterFilled = griddata(known_coords, known_values, coords, method='linear').reshape(
+            heightRasterSize)
 
-        # 使用插值方法为未知值的栅格赋值
-        interpolated_values = griddata(known_coords, known_values, unknown_coords, method='cubic')
-
-        # 在插值后检查 NaN 值
-        nan_indices = np.isnan(interpolated_values)
-
-        # 如果存在 NaN 值，使用最近邻插值方法处理它们
-        if np.any(nan_indices):
-            interpolated_values[nan_indices] = griddata(known_coords, known_values, unknown_coords[nan_indices],
-                                                        method='nearest')
-
-        # 更新DSM中未知值的栅格
-        self.dsm[no_data_indices] = np.nan_to_num(interpolated_values, nan=np.min(self.dsm))
-
+        # 如果您还想要填充边缘的NaN值，可以使用其他方法，如“nearest”插值
+        filled_matrix = griddata(known_coords, known_values, coords, method='nearest').reshape(heightRasterSize)
+        self.heightRasterFilled[np.isnan(self.heightRasterFilled)] = filled_matrix[np.isnan(self.heightRasterFilled)]
         # 更新归一化的高程栅格图像
-        self.normalized_dsm = (self.dsm - np.min(self.dsm)) / (np.max(self.dsm) - np.min(self.dsm))
+        self.normalizedHeightRaster[self.heightRasterFilled > 0] = (self.heightRasterFilled[
+                                                                        self.heightRasterFilled > 0] -
+                                                                    np.min(self.heightRasterFilled[
+                                                                               self.heightRasterFilled > 0])) / \
+                                                                   (np.max(self.heightRasterFilled[
+                                                                               self.heightRasterFilled > 0]) -
+                                                                    np.min(self.heightRasterFilled[
+                                                                               self.heightRasterFilled > 0]))
 
     def save_dsm_image(self, file_path):
         """
         将高程栅格图像保存为灰度图像
         """
-        dsm_image_array = (self.normalized_dsm * 255).astype(np.uint8)
+        dsm_image_array = (self.normalizedHeightRaster * 255).astype(np.uint8)
         dsm_image = Image.fromarray(dsm_image_array, mode='L')
         dsm_image.save(file_path)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app.libs.laz_process.dsm_generator import DSM
 
 if __name__ == '__main__':
-    dsm = DSM(laz_files_dir='./test/', step_size=0.09)
-    # dsm.fill_no_data()
+    dsm = DSM(laz_files_path='E:/Kilo/Codes/Python/ffGraduationProject/app/data/lazs/points1.laz', step_size=0.1)
     dsm.save_dsm_image(file_path='test.tif')
+    dsm.fill_no_data()
+    dsm.save_dsm_image(file_path='testFilled.tif')
